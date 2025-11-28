b21f8555657d121fa164a6f9786d1f6e38fbd03b
StevenXuf <chasefreedom@163.com>
2025-11-28 18:20:35 +0100

    add mllms

--- retrieval.py
+++ retrieval.py
@@ -2,18 +2,16 @@ import torch
 import fire
 import os
 import time
-import numpy as np
 
-from PIL import Image
 from tqdm import tqdm
 from diffusers import StableDiffusionXLInstructPix2PixPipeline, AutoPipelineForImage2Image, AutoPipelineForText2Image
-from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor, GenerationConfig, set_seed
+from transformers import Qwen2_5_VLForConditionalGeneration, LlavaForConditionalGeneration, DeepseekVLForConditionalGeneration, AutoProcessor, GenerationConfig, set_seed
 
 from figures import show_tensor_images
 from feature_extraction import get_feature_extractor, get_metrics
 from dataloaders import get_dataloader
 from text_to_image_and_text import fashioniq_eval, genecis_eval, generate_texts, extract_text_features, extract_image_features, store_top_k
-from prompts import get_composed_prompts, get_target_prompts
+from prompts import get_composed_prompts, get_target_prompts, get_composed_prompts_other_mllm
 from utils import get_default_config, convert_pil_to_tensor, transform_image, delete_models, get_gpu_memory
 
 
@@ -74,6 +72,56 @@ def get_test_loader(cfg, dataset_name, split, extractor, img_batch_size):
         )
     return test_loader
 
+def load_mllm(cfg, **kwargs):
+    ### Text Generation Parameters
+    
+    temperature = kwargs.get('temperature', cfg['TEXT-GENERATION']['GLOBAL']['TEMPERATURE'])
+    top_p = kwargs.get('top_p', cfg['TEXT-GENERATION']['GLOBAL']['TOP_P'])
+    llm_top_k = kwargs.get('llm_top_k', cfg['TEXT-GENERATION']['GLOBAL']['TOP_K'])
+    max_new_tokens = kwargs.get('max_new_tokens', cfg['TEXT-GENERATION']['GLOBAL']['MAX_NEW_TOKENS'])
+
+    device = torch.device(f"cuda:{kwargs['device']}") if kwargs.get('device') is not None else torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    gen_config = GenerationConfig(do_sample=True,
+                                    temperature=temperature,
+                                    top_p=top_p,
+                                    top_k=llm_top_k,
+                                    max_new_tokens=max_new_tokens
+                                )
+    
+    mllm = kwargs.get('mllm', cfg['GENERAL']['MLLM']).upper()
+    before = get_gpu_memory(device)
+    if mllm == 'QWEN-7B' or mllm == 'QWEN-3B':
+        model_id = cfg['TEXT-GENERATION'][mllm]['MODEL_NAME']
+        text_generation_model = Qwen2_5_VLForConditionalGeneration.from_pretrained(model_id, 
+                                                                                torch_dtype=torch.bfloat16, 
+                                                                                device_map={"": device}, 
+                                                                                # attn_implementation='flash_attention_2'
+                                                                                ).to(device)
+    elif mllm == 'LLAVA':
+        model_id = cfg['TEXT-GENERATION'][mllm]['MODEL_NAME']
+        text_generation_model = LlavaForConditionalGeneration.from_pretrained(model_id, 
+                                                                            torch_dtype=torch.bfloat16, 
+                                                                            device_map={"": device}
+                                                                            ).to(device)
+    elif mllm == 'DEEPSEEK':
+        model_id = cfg['TEXT-GENERATION']['DEEPSEEK']['MODEL_NAME']
+        text_generation_model = DeepseekVLForConditionalGeneration.from_pretrained(model_id, 
+                                                                            torch_dtype=torch.bfloat16, 
+                                                                            device_map={"": device}
+                                                                            ).to(device)
+    else:
+        raise ValueError(f"Unsupported MLLM: {mllm}. Should be one of ['QWEN', 'LLAVA', 'DEEPSEEK']")
+    after = get_gpu_memory(device)
+    print(f"Meomeroy taken by {mllm.upper()}: {(after - before) / 1024**2: .2f} MB\n")
+    text_generation_model.eval()
+    print(f"Using {text_generation_model.__class__.__name__} for text generation with temperature={temperature}, top_p={top_p}, top_k={llm_top_k}, max_new_tokens={max_new_tokens}")
+    processor = AutoProcessor.from_pretrained(model_id, 
+                                            padding_side='left', 
+                                            use_fast=True
+                                            )
+    return gen_config, processor, text_generation_model
+
 def main(cfg, **kwargs):
     ### General Parameters
     top_k = kwargs.get('top_k', cfg['GENERAL']['TOP_K'])
@@ -85,14 +133,8 @@ def main(cfg, **kwargs):
     pretrained = kwargs.get('pretrained', cfg[extractor]['PRETRAINED'])
     dataset_name = kwargs.get('dataset', cfg['GENERAL']['DATASET'])
     split = kwargs.get('split', cfg['GENERAL']['SPLIT'])
+    mllm = kwargs.get('mllm', cfg['GENERAL']['MLLM'])
     print(f"Using {extractor} for feature extraction on {dataset_name} ({split})")
-
-    ### Text Generation Parameters
-    model_id = cfg['TEXT-GENERATION']['QWEN']['MODEL_NAME']
-    temperature = kwargs.get('temperature', cfg['TEXT-GENERATION']['GLOBAL']['TEMPERATURE'])
-    top_p = kwargs.get('top_p', cfg['TEXT-GENERATION']['GLOBAL']['TOP_P'])
-    llm_top_k = kwargs.get('llm_top_k', cfg['TEXT-GENERATION']['GLOBAL']['TOP_K'])
-    max_new_tokens = kwargs.get('max_new_tokens', cfg['TEXT-GENERATION']['GLOBAL']['MAX_NEW_TOKENS'])
 
     feature_extraction_model, img_preprocess, tokenizer = get_feature_extractor(cfg, 
                                                                                 extractor=extractor, 
@@ -105,12 +147,12 @@ def main(cfg, **kwargs):
     if task.startswith('img2'):
         ### Image Generation Parameters
         seed = kwargs.get('seed', cfg['GENERAL']['SEED'])
-        image_gen_model_name = kwargs.get('image_gen_model', cfg['GENERAL']['IMAGE_GEN_MODEL'])
+        image_gen_model_name = kwargs.get('image_gen_model', cfg['GENERAL']['IMAGE_GEN_MODEL']).upper()
         image_size = cfg['IMAGE-GENERATION'][image_gen_model_name]['IMAGE_SIZE']
         n_infer_steps = kwargs.get('n_infer_steps', cfg['IMAGE-GENERATION'][image_gen_model_name]['NUM_INFERENCE_STEPS'])
         image_guidance_scale = kwargs.get('image_guidance_scale', cfg['IMAGE-GENERATION'][image_gen_model_name]['IMAGE_GUIDANCE_SCALE'])
         guidance_scale = kwargs.get('guidance_scale', cfg['IMAGE-GENERATION'][image_gen_model_name]['GUIDANCE_SCALE'])
-        use_llm = kwargs.get('use_llm', cfg['GENERAL']['USE_LLM'])
+        use_mllm = kwargs.get('use_mllm', cfg['GENERAL']['USE_MLLM'])
 
         img_transform_for_generation = transform_image(image_size)
         generator = torch.Generator(device="cuda").manual_seed(seed)
@@ -131,30 +173,13 @@ def main(cfg, **kwargs):
         image_generation_model.enable_attention_slicing()
         print(f"Using {image_generation_model.__class__.__name__} with params: n_infer_steps={n_infer_steps}, image_guidance_scale={image_guidance_scale}, guidance_scale={guidance_scale}")
 
-        store_path = os.path.join(cfg['IMAGE-GENERATION'][image_gen_model_name]['OUTPUT_DIR'], f'Qwen_{use_llm}_{dataset_name}_{split}_{extractor}_{n_infer_steps}_{image_guidance_scale}_{guidance_scale}')
+        img_folder = f'{mllm}_{dataset_name}_{split}_{n_infer_steps}_{image_guidance_scale}_{guidance_scale}' if use_mllm.lower() == 'yes' else f'NoMLLM_{dataset_name}_{split}_{n_infer_steps}_{image_guidance_scale}_{guidance_scale}'
+        store_path = os.path.join(cfg['IMAGE-GENERATION'][image_gen_model_name]['OUTPUT_DIR'], img_folder)
         if not os.path.exists(store_path):
             os.makedirs(store_path)
 
-    gen_config = GenerationConfig(do_sample=True,
-                                    temperature=temperature,
-                                    top_p=top_p,
-                                    top_k=llm_top_k,
-                                    max_new_tokens=max_new_tokens
-                                )
-    before = get_gpu_memory(device)
-    text_generation_model = Qwen2_5_VLForConditionalGeneration.from_pretrained(model_id, 
-                                                                            torch_dtype=torch.bfloat16, 
-                                                                            device_map={"": device}, 
-                                                                            # attn_implementation='flash_attention_2'
-                                                                            ).to(device)
-    after = get_gpu_memory(device)
-    print(f"Meomeroy taken by {cfg['GENERAL']['MLLM']}: {(after - before) / 1024**2: .2f} MB\n")
-    text_generation_model.eval()
-    print(f"Using {text_generation_model.__class__.__name__} for text generation with temperature={temperature}, top_p={top_p}, top_k={llm_top_k}, max_new_tokens={max_new_tokens}")
-    processor = AutoProcessor.from_pretrained(model_id, 
-                                            padding_side='left', 
-                                            use_fast=True
-                                            )
+    gen_config, processor, text_generation_model = load_mllm(cfg, **kwargs)
+    
     dataloader = get_dataloader(cfg, 
                                 split=split.lower(), 
                                 mode='relative',
@@ -177,7 +202,7 @@ def main(cfg, **kwargs):
         img_subset_ids = []
         img_subset_feat = []
     with torch.no_grad():
-        start_time_query = time.time()
+        total_time_query = 0.0
         for i, batch in tqdm(enumerate(dataloader), desc=f"Extracting query features for {task}", total=len(dataloader)):
             reference_pil = batch['reference_pil']
             caption = batch['caption']
@@ -190,13 +215,23 @@ def main(cfg, **kwargs):
 
             if task.startswith('img2'):
                 reference_img = batch['reference_img']
-                if use_llm == 'yes':
-                    composed_messages = list(map(lambda x: get_composed_prompts(dataset_name, *x),zip(reference_pil, caption)))
+                if use_mllm == 'yes':
+                    if mllm.lower() == 'qwen-3b' or mllm.lower() == 'qwen-7b':
+                        composed_messages = {'texts':list(map(lambda x: get_composed_prompts(dataset_name, *x),zip(reference_pil, caption))),
+                                             'images':[]
+                                            }
+                    elif mllm.lower() == 'llava' or mllm.lower() == 'deepseek':
+                        composed_messages = {'texts':list(map(lambda cap: get_composed_prompts_other_mllm(mllm, cap), caption)),
+                                             'images':reference_pil
+                                            }
+                    else:
+                        raise ValueError(f"Unsupported MLLM: {mllm}. Should be one of ['QWEN', 'LLAVA', 'DEEPSEEK']")
                     caption = generate_texts(composed_messages, gen_config, processor, text_generation_model)
                     print(caption)
 
                 show_tensor_images(reference_img, num_images=reference_img.size(0), file_path=os.path.join(store_path,f"reference_image_grid_{i}.png"))
                 print(f"Generating target images for batch {i+1}")
+                start_time_batch = time.time()
                 if image_gen_model_name.upper() == 'SDXL-INSTRUCTPIX2PIX':
                     generated_target_images = image_generation_model(
                         prompt=caption,
@@ -220,6 +255,8 @@ def main(cfg, **kwargs):
                         guidance_scale=guidance_scale,
                         generator=generator
                         ).images
+                end_time_batch = time.time()
+                total_time_query += end_time_batch - start_time_batch
                 generated_target_image_tensor = torch.stack(convert_pil_to_tensor(generated_target_images))
                 show_tensor_images(generated_target_image_tensor, num_images=generated_target_image_tensor.size(0), file_path=os.path.join(store_path,f"generated_target_image_grid_{i}.png"))
 
@@ -227,15 +264,27 @@ def main(cfg, **kwargs):
 
             elif task.startswith('txt2'):
                 composed_messages = list(map(lambda x: get_composed_prompts(dataset_name, *x),zip(reference_pil, caption)))
+                start_time_batch = time.time()
+                if mllm.lower() == 'qwen-3b' or mllm.lower() == 'qwen-7b':
+                    composed_messages = {'texts':list(map(lambda x: get_composed_prompts(dataset_name, *x),zip(reference_pil, caption))),
+                                             'images':[]
+                                        }
+                elif mllm.lower() == 'llava' or mllm.lower() == 'deepseek':
+                    composed_messages = {'texts':list(map(lambda cap: get_composed_prompts_other_mllm(mllm, cap), caption)),
+                                            'images':reference_pil
+                                        }
+                else:
+                    raise ValueError(f"Unsupported MLLM: {mllm}. Should be one of ['QWEN', 'LLAVA', 'DEEPSEEK']")
                 composed_descriptions = generate_texts(composed_messages, gen_config, processor, text_generation_model)
+                end_time_batch = time.time()
+                total_time_query += end_time_batch - start_time_batch
                 print(composed_descriptions)
                 query_feat.append(extract_text_features(composed_descriptions, extractor, tokenizer, feature_extraction_model))
             else:
                 raise ValueError(f"Unsupported task: {task}. Should be one of ['txt2img', 'txt2txt', 'img2img', 'img2txt']")
-        end_time_query = time.time()
         print(f"Finished extracting query features for {task} on {dataset_name}.".upper())
         print("=" * 50)
-        print(f"Total time for extracting query features: {end_time_query - start_time_query: .2f} seconds\n Per sample time: {(end_time_query - start_time_query)/len(dataloader.dataset): .2f} seconds\n")
+        print(f"Total time for extracting query features: {total_time_query: .2f} seconds\nPer sample time: {total_time_query/len(dataloader.dataset): .2f} seconds\n")
         print("=" * 50)
         
         test_loader = get_test_loader(cfg, dataset_name, split, extractor, img_batch_size)
@@ -323,8 +372,7 @@ def main(cfg, **kwargs):
                                       metrics=metric
                                     )
             print("=" * 50)
-            print(f'|{metric.upper()}@{k}: {metric_val:.2f}% when using generated description ---> target images |\n')
-            print("=" * 50)
+            print(f'{metric.upper()}@{k}: {metric_val:.2f}% when using generated description ---> target images\n')
             
             res.append(metric_val.item())
         return sum(res)/len(res)
